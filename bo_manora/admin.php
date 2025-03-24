78d3df85005c6a0095dfc56eddd63df75f0937db
MrLuciana <mrbiiigs17@gmail.com>
2025-03-24 16:52:29 +0700

    Fix formatting in admin.php for better readability

--- bo_manora/admin.php
+++ bo_manora/admin.php
@@ -348,8 +348,8 @@ $sql .= " LIMIT $start,$rows_per_page";
                                                         <?php } ?>
                                                     </td>
                                                 </tr>
-                                        <?php } $i++;
-                                        } ?>
+                                        <?php } 
+                                        } $i++;?>
                                     </tbody>
                                 </table>
                             </div>
